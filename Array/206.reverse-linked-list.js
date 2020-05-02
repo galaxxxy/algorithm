179f0612e9f98baa698f991a82db3a47e68a4bfe
galaxxxy <2511030361@qq.com>
2020-05-02 19:50:43 +0800

    Create 206.reverse-linked-list.js

---
+++ Array/206.reverse-linked-list.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=206 lang=javascript
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  let cur = head,
+      prev = null;
+  while (cur !== null) {
+    const cnext = cur.next;
+    cur.next = prev;
+    prev = cur;
+    cur = cnext;
+  }
+  return prev;
+};
+// @lc code=end
+
